e4e1ab7b1fa0868c448e6c26523daf9aa39a8e6a
Scott Graham <scottmg@chromium.org>
2016-09-02 09:34:30 -0700

    Mark histogram arguments as unused to avoid warnings
    When embedding histogram macros, (void) the arguments to avoid "variable
    initialized but unused" warnings.
    
    R=mark@chromium.org
    BUG=crashpad:100
    
    Change-Id: I97804a01a8b3ca7e786a970e235c09d554e3c3c6

--- base/metrics/histogram_macros.h
+++ base/metrics/histogram_macros.h
@@ -9,28 +9,36 @@
 // base/metrics/histogram_macros.h. This allows us to instrument the Crashpad
 // code as necessary, while not affecting out-of-Chromium builds.
 
-#define UMA_HISTOGRAM_TIMES(name, sample)
-#define UMA_HISTOGRAM_MEDIUM_TIMES(name, sample)
-#define UMA_HISTOGRAM_LONG_TIMES(name, sample)
-#define UMA_HISTOGRAM_LONG_TIMES_100(name, sample)
-#define UMA_HISTOGRAM_CUSTOM_TIMES(name, sample, min, max, bucket_count)
+#define UNUSED(x) (void)(x)
 
-#define UMA_HISTOGRAM_COUNTS(name, sample)
-#define UMA_HISTOGRAM_COUNTS_100(name, sample)
-#define UMA_HISTOGRAM_COUNTS_1000(name, sample)
-#define UMA_HISTOGRAM_COUNTS_10000(name, sample)
-#define UMA_HISTOGRAM_CUSTOM_COUNTS(name, sample, min, max, bucket_count)
+#define UMA_HISTOGRAM_TIMES(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_MEDIUM_TIMES(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_LONG_TIMES(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_LONG_TIMES_100(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_CUSTOM_TIMES(name, sample, min, max, bucket_count) \
+  UNUSED(name), UNUSED(sample), UNUSED(min), UNUSED(max), UNUSED(bucket_count)
 
-#define UMA_HISTOGRAM_MEMORY_KB(name, sample)
-#define UMA_HISTOGRAM_MEMORY_MB(name, sample)
-#define UMA_HISTOGRAM_MEMORY_LARGE_MB(name, sample)
+#define UMA_HISTOGRAM_COUNTS(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_COUNTS_100(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_COUNTS_1000(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_COUNTS_10000(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_CUSTOM_COUNTS(name, sample, min, max, bucket_count) \
+  UNUSED(name), UNUSED(sample), UNUSED(min), UNUSED(max), UNUSED(bucket_count)
 
-#define UMA_HISTOGRAM_PERCENTAGE(name, under_one_hundred)
+#define UMA_HISTOGRAM_MEMORY_KB(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_MEMORY_MB(name, sample) UNUSED(name), UNUSED(sample)
+#define UMA_HISTOGRAM_MEMORY_LARGE_MB(name, sample) UNUSED(name), UNUSED(sample)
 
-#define UMA_HISTOGRAM_BOOLEAN(name, sample)
+#define UMA_HISTOGRAM_PERCENTAGE(name, under_one_hundred) \
+  UNUSED(name), UNUSED(under_one_hundred)
 
-#define UMA_HISTOGRAM_ENUMERATION(name, sample, boundary_value)
-#define UMA_STABILITY_HISTOGRAM_ENUMERATION(name, sample, boundary_value)
-#define UMA_HISTOGRAM_CUSTOM_ENUMERATION(name, sample, custom_ranges)
+#define UMA_HISTOGRAM_BOOLEAN(name, sample) UNUSED(name), UNUSED(sample)
+
+#define UMA_HISTOGRAM_ENUMERATION(name, sample, boundary_value) \
+  UNUSED(name), UNUSED(sample), UNUSED(boundary_value)
+#define UMA_STABILITY_HISTOGRAM_ENUMERATION(name, sample, boundary_value) \
+  UNUSED(name), UNUSED(sample), UNUSED(boundary_value)
+#define UMA_HISTOGRAM_CUSTOM_ENUMERATION(name, sample, custom_ranges) \
+  UNUSED(name), UNUSED(sample), UNUSED(custom_ranges)
 
 #endif  // MINI_CHROMIUM_BASE_METRICS_HISTOGRAM_MACROS_H_
