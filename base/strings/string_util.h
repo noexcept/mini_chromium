26efaffa37fe8751fa4214dfbc9c21238f391728
Robert Sesek <rsesek@chromium.org>
2017-08-14 13:49:46 -0400

    base/strings/string_util.h: Add strlcpy.
    Bug: crashpad:192
    Change-Id: Ia1c7fa34fce0a83e54768d1e894c231f0dbcd26d

--- base/strings/string_util.h
+++ base/strings/string_util.h
@@ -13,6 +13,15 @@ namespace base {
 int vsnprintf(char* buffer, size_t size, const char* format, va_list arguments)
     PRINTF_FORMAT(3, 0);
 
+// BSD-style safe and consistent string copy functions.
+// Copies |src| to |dst|, where |dst_size| is the total allocated size of |dst|.
+// Copies at most |dst_size|-1 characters, and always NULL terminates |dst|, as
+// long as |dst_size| is not 0.  Returns the length of |src| in characters.
+// If the return value is >= dst_size, then the output was truncated.
+// NOTE: All sizes are in number of characters, NOT in bytes.
+size_t strlcpy(char* dst, const char* src, size_t dst_size);
+size_t wcslcpy(wchar_t* dst, const wchar_t* src, size_t dst_size);
+
 }  // namespace base
 
 #if defined(OS_WIN)
