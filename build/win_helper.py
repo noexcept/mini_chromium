b9b2eaf120f675833d88c99df6d91dd7a3b2e4cc
Scott Graham <scottmg@chromium.org>
2017-12-06 13:25:35 -0800

    gn, win: Add copy and solink_module tools to win toolchain
    Bug: crashpad:79
    Change-Id: Ide00473684a46f3b7aa78ba9ba37c1012c681640

--- build/win_helper.py
+++ build/win_helper.py
@@ -37,6 +37,32 @@ class WinTool(object):
         print line
     return link.returncode
 
+  def ExecRecursiveMirror(self, source, dest):
+    """Emulation of rm -rf out && cp -af in out."""
+    if os.path.exists(dest):
+      if os.path.isdir(dest):
+        def _on_error(fn, path, dummy_excinfo):
+          # The operation failed, possibly because the file is set to
+          # read-only. If that's why, make it writable and try the op again.
+          if not os.access(path, os.W_OK):
+            os.chmod(path, stat.S_IWRITE)
+          fn(path)
+        shutil.rmtree(dest, onerror=_on_error)
+      else:
+        if not os.access(dest, os.W_OK):
+          # Attempt to make the file writable before deleting it.
+          os.chmod(dest, stat.S_IWRITE)
+        os.unlink(dest)
+
+    if os.path.isdir(source):
+      shutil.copytree(source, dest)
+    else:
+      shutil.copy2(source, dest)
+      # Try to diagnose crbug.com/741603
+      if not os.path.exists(dest):
+        raise Exception("Copying of %s to %s failed" % (source, dest))
+
+
   def ExecStamp(self, path):
     """Simple stamp command."""
     open(path, 'w').close()
