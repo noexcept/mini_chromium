9271a3c9d581875d65e25be5727df0decb91c096
Mark Mentovai <mark@chromium.org>
2017-12-15 17:46:41 -0500

    gn, mac: Honor mac_sdk_min, sysroot, and mac_deployment_target
    This is bit more flexible (and probably straightforward) than how
    Chromium deals with things. The SDK is selected based on the first
    non-empty GN argument out of sysroot, mac_sdk, and mac_sdk_min.
    Regardless of how the SDK is selected, if mac_sdk is non-empty, the SDK
    must be that exact version, and if mac_sdk_min is non-empty, the SDK
    must be at least that version. It’s possible for all three to be empty,
    which will result in the system’s default SDK being chosen, without any
    additional constraints. This is the current default.
    
    Similarly, mac_deployment_target may be empty, which allows the
    toolchain to choose the deployment target. The current default is to
    explicitly specify 10.9 as the deployment target.
    
    Unlike Chromium, this ought to work on systems with only the Command
    Line Tools package installed, but no Xcode. Also unlike Chromium, no
    provision has been made (yet?) for the hermetic tools package.
    
    The sysroot logic for macOS and Fuchsia has been consolidated, although
    there’s probably more room for improvement.
    
    Bug: crashpad:79
    Change-Id: I1df56921bd68934b6c4671707ca47842b58047fe

--- build/find_mac_sdk.py
+++ build/find_mac_sdk.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 # Copyright 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -6,16 +7,111 @@
 
 from __future__ import print_function
 
+import argparse
+import distutils.version
+import os.path
+import re
 import subprocess
 import sys
 
+PLATFORM = 'macosx'
+
+
+def _AsVersion(string):
+  return distutils.version.StrictVersion(string)
+
+
+def _RunXCRun(args, sdk=None):
+  xcrun_args = ['xcrun']
+  if sdk is not None:
+    xcrun_args.extend(['--sdk', sdk])
+  xcrun_args.extend(args)
+  return subprocess.check_output(xcrun_args).decode('utf-8').rstrip()
+
+
+def _SDKPath(sdk=None):
+  return _RunXCRun(['--show-sdk-path'], sdk)
+
+
+def _SDKVersion(sdk=None):
+  return _AsVersion(_RunXCRun(['--show-sdk-version'], sdk))
+
+
+def _DidNotMeetMinimum(params):
+  raise Exception('no suitable SDK found', params)
+
+
+def _FindSDKMinimumVersion(minimum_sdk_version_str):
+  minimum_sdk_version = _AsVersion(minimum_sdk_version_str)
+
+  # Try the SDKs that Xcode knows about.
+  xcodebuild_showsdks_subprocess = subprocess.Popen(['xcodebuild', '-showsdks'],
+                                                    stdout=subprocess.PIPE,
+                                                    stderr=subprocess.PIPE)
+  xcodebuild_showsdks_output = (
+      xcodebuild_showsdks_subprocess.communicate()[0].decode('utf-8'))
+  if xcodebuild_showsdks_subprocess.returncode == 0:
+    # Collect strings instead of version objects to preserve the precise
+    # format used to identify each SDK.
+    sdk_version_strs = []
+    for line in xcodebuild_showsdks_output.splitlines():
+      match = re.match(
+          '\t((Mac )?OS X|macOS) .+-sdk ' + re.escape(PLATFORM) + '(.+)$', line)
+      if match:
+        sdk_version_str = match.group(3)
+        if _AsVersion(sdk_version_str) >= minimum_sdk_version:
+          sdk_version_strs.append(sdk_version_str)
+
+    if len(sdk_version_strs) == 0:
+      _DidNotMeetMinimum({'minimum': minimum_sdk_version_str})
+    sdk_version_str = sorted(sdk_version_strs, key=_AsVersion)[0]
+    sdk_path = _SDKPath(PLATFORM + sdk_version_str)
+    sdk_version = _AsVersion(sdk_version_str)
+  else:
+    # Xcode may not be installed. If the command-line tools are installed, use
+    # the system’s default SDK if it meets the requirements.
+    sdk_path = _SDKPath()
+    sdk_version = _SDKVersion()
+    if sdk_version < minimum_sdk_version:
+      _DidNotMeetMinimum({'minimum': minimum_sdk_version_str})
+
+  return (sdk_path, sdk_version)
+
 
 def main(args):
-  if len(args) != 0:
-    print('usage: find_mac_sdk.py', file=sys.stderr)
-    return 1
+  parser = argparse.ArgumentParser(description='Find an appropriate macOS SDK')
+  parser.add_argument('--exact', help='an exact SDK version to find')
+  parser.add_argument('--minimum', help='the minimum SDK version to find')
+  parser.add_argument('--path', help='a known SDK path to validate')
+  parsed = parser.parse_args(args)
 
-  subprocess.check_call(['xcrun', '--show-sdk-path'])
+  if parsed.path is not None:
+    sdk_path = parsed.path
+    sdk_version = _SDKVersion(sdk_path)
+  elif parsed.exact is None and parsed.minimum is None:
+    # Use the system’s default SDK.
+    sdk_path = _SDKPath()
+    sdk_version = _SDKVersion()
+  elif parsed.exact is not None:
+    sdk_path = _SDKPath(PLATFORM + parsed.exact)
+    sdk_version = _SDKVersion(PLATFORM + parsed.exact)
+  else:
+    (sdk_path, sdk_version) = _FindSDKMinimumVersion(parsed.minimum)
+
+  # This is redundant if _FindSDKMinimumVersion() was called, but it’s necessary
+  # if --minimum was specified along with --exact or --path.
+  if parsed.minimum is not None and sdk_version < _AsVersion(parsed.minimum):
+    _DidNotMeetMinimum({'exact': parsed.exact,
+                        'minimum': parsed.minimum,
+                        'path': parsed.path})
+
+  # Nobody wants trailing slashes. This is true even if “/” is the SDK: it’s
+  # better to return an empty string, which will be interpreted as “no sysroot.”
+  sdk_path.rstrip(os.path.sep)
+
+  print(sdk_path)
+  print(sdk_version)
+
   return 0
 
 
