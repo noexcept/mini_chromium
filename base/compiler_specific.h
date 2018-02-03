fddfab0107414afd6e7176e7149c160684be89f2
Scott Graham <scottmg@chromium.org>
2018-02-02 16:54:36 -0800

    Add -Wimplicit-fallthrough when building on clang
    Bug: chromium:807632
    Change-Id: I02904913dd32c67b578f591fd0244a7adca4907b

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -77,4 +77,11 @@
 #define HAS_FEATURE(FEATURE) 0
 #endif
 
+// Macro for telling -Wimplicit-fallthrough that a fallthrough is intentional.
+#if defined(__clang__)
+#define FALLTHROUGH [[clang::fallthrough]]
+#else
+#define FALLTHROUGH
+#endif
+
 #endif  // MINI_CHROMIUM_BASE_COMPILER_SPECIFIC_H_
