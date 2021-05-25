fbdc4590f1ee0062fb7b800f987d9271f1f001f5
Lei Zhang <thestig@chromium.org>
2021-05-25 12:34:37 -0700

    Rename base/stl_util.h to base/cxx17_backports.h.
    This is the mini_chromium equivalent of https://crrev.com/885290.
    
    Change-Id: I574de911428353357b5410e87dcaa6adb983156d

--- base/cxx17_backports.h
+++ base/cxx17_backports.h
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MINI_CHROMIUM_BASE_STL_UTIL_H_
-#define MINI_CHROMIUM_BASE_STL_UTIL_H_
-
-#include <sys/types.h>
+#ifndef MINI_CHROMIUM_BASE_CXX17_BACKPORTS_H_
+#define MINI_CHROMIUM_BASE_CXX17_BACKPORTS_H_
 
 namespace base {
 
@@ -16,4 +14,4 @@ constexpr size_t size(const T (&array)[N]) noexcept {
 
 }  // namespace base
 
-#endif  // MINI_CHROMIUM_BASE_STL_UTIL_H_
+#endif  // MINI_CHROMIUM_BASE_CXX17_BACKPORTS_H_
