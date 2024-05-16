f23f05fe61075d688ef7733bf3517f61fcf2f568
Peter Boström <pbos@chromium.org>
2024-05-16 10:37:10 -0700

    Add NOTREACHED_IN_MIGRATION()
    This is to be used by crashpad temporarily NOTREACHED() migrates to be
    [[noreturn]].
    
    Bug: chromium:40580068
    Change-Id: Ib3490671364190e9917218897f5627a0d79b8053

--- base/notreached.h
+++ base/notreached.h
@@ -7,6 +7,16 @@
 
 #include "base/check.h"
 
+// TODO(crbug.com/40580068): Redefine NOTREACHED() to be the [[noreturn]] once
+// crashpad and chromium has migrated off of the non-noreturn version. This is
+// easiest done by defining it as std::abort() as crashpad currently doesn't
+// stream arguments to it. For a more complete implementation we should
+// LOG(FATAL) but that doesn't work until mini_chromium's LOG(FATAL) is properly
+// understood as [[noreturn]].
 #define NOTREACHED() DCHECK(false)
+
+// TODO(crbug.com/40580068): Remove this once the NotReachedIsFatal experiment
+// has been rolled out in chromium.
+#define NOTREACHED_IN_MIGRATION() DCHECK(false)
 
 #endif  // MINI_CHROMIUM_BASE_NOTREACHED_H_
